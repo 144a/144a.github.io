1c034d0ac74cead24695aea946be659dd8865648
144a <andy.gatza@gmail.com>
2022-03-04 22:37:29 -0600

    (AGV) Andy Gatza: Completed 1st Website

---
+++ src/data.js
@@ -0,0 +1,89 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": ""
+        },
+        {
+            "icon": "github-circled",
+            "url": "http://github/144a"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": ""
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/in/andy-gatza-5884261b7/http://linkedin"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "5bdff4b8-1e31-44bf-b8d2-00a23a046380",
+            "title": "Manufacture Technician - Abbott",
+            "timeline": "Jan 2016 - Present",
+            "description": "Perform all types of complex demand and preventative maintenance and assembly work on semi-automated manufacturing equipment, including programming, troubleshooting, and repair of inkjet printers, sealing equipment, pouching equipment, vision systems, labeling, and feeding equipment along with other equipment of similar type. \n",
+            "responsibilities": [
+                "Developed custom software to interface with a variety of proprietary devices within tight time constraints. ",
+                "Prototyped a computer vision quality assurance system to ensure proper product quality, solving a variety of issues within the production line."
+            ]
+        },
+        {
+            "__key": "7ceb3c4f-728e-4b9a-a50b-491ced913ca8",
+            "title": "Database Developer - CB Therapies",
+            "timeline": "Feb 2015 - Dec 2015",
+            "responsibilities": [
+                "Developed a new SQL database to support all functionality needed to maintain patient data within HIPPA regulations and other related specifications",
+                "Serviced and supported the existing database, providing both scheduled maintenance and immediate demand maintenance."
+            ],
+            "description": ""
+        }
+    ],
+    "name": "Andy Gatza",
+    "header": "Hi, I'm M̶e̶a̶t̶l̶o̶a̶f̶ Andy Gatza ",
+    "cta": {
+        "label": "Get my resume",
+        "url": "resume.pdf"
+    },
+    "testimonials": [],
+    "title": "Andy Gatza",
+    "projects": [
+        {
+            "__key": "befb2c85-2a4c-48ea-a651-9d4bb0c8d445",
+            "name": "Product Vision Inspection System",
+            "description": "Designed and developed a vision system to determine the quality and physical placement of a strip relative to the empty space on a card.",
+            "tags": [
+                "OpenCV",
+                "Python",
+                "C++",
+                "Raspberry Pi",
+                "Numpy"
+            ],
+            "alt": "",
+            "img": "vision.png",
+            "url": "https://github.com/144a/BlueLineDetect"
+        },
+        {
+            "__key": "d114efaa-8477-4692-a289-9d23f43a1aa0",
+            "name": "String Art Generator",
+            "description": "A program that generates a string-approximation of an image using differential integrals.",
+            "tags": [
+                "Processing",
+                "Java"
+            ],
+            "img": "stringart.png",
+            "url": "https://github.com/144a/stringArtGenerator"
+        }
+    ],
+    "email": "andy.gatza@gmail.com",
+    "description": "I'm a college senior currently studying Computer Science at Depaul University and work as a Manufacturing Technician at Abbott. I am an excellent team player and leader, and am known for being eclectic and unique in both my professional work and in my vivid personality. I specialize in Computer Vision development, with a heavy focus in low-cost and efficient projects allowing for robust and expandable implementations. When not programming, I enjoy repairing old electronics, collecting video games, and producing videos with old professional broadcast equipment.",
+    "footerTitle": "Software Engineer"
+}
